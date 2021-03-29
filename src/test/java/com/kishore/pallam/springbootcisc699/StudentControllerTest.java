6642f63c6cfc6efb2c4f36f389683f010d3912ca
Krishna Kishore pallamreddy <krishnakishorepallamreddy@krishnas-mbp.lan>
2021-03-28 22:48:02 -0500

    added Hello world

--- src/test/java/com/kishore/pallam/springbootcisc699/StudentControllerTest.java
+++ src/test/java/com/kishore/pallam/springbootcisc699/StudentControllerTest.java
@@ -21,8 +21,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import com.kishore.pallam.springbootcisc699.dto.Student;
-import com.kishore.pallam.springbootcisc699.service.StudentService;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
@@ -31,55 +34,20 @@ public class StudentControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
-	private StudentService studentService;
+
 
 	private static ObjectMapper mapper = new ObjectMapper();
 
 	@Test
 	public void testGetExample() throws Exception {
-		List<Student> students = new ArrayList<>();
-		Student student = new Student();
-		student.setId(1);
-		student.setName("Arun");
-		students.add(student);
-		Mockito.when(studentService.getStudents()).thenReturn(students);
-		mockMvc.perform(get("/getMapping")).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
-				.andExpect(jsonPath("$[0].name", Matchers.equalTo("Michael")));
+		
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(MockMvcResultMatchers.status().is(200))
+        .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+        .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"))
+        .andExpect(MockMvcResultMatchers.header().string("Content-Length", "11"));
+    
 	}
 
-	@Test
-	public void testPostExample() throws Exception {
-		Student student = new Student();
-		student.setId(1);
-		student.setName("Arun");
-		Mockito.when(studentService.saveStudent(ArgumentMatchers.any())).thenReturn(student);
-		String json = mapper.writeValueAsString(student);
-		mockMvc.perform(post("/postMapping").contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-				.content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
-				.andExpect(jsonPath("$.id", Matchers.equalTo(1)))
-				.andExpect(jsonPath("$.name", Matchers.equalTo("Michael")));
-	}
-
-	@Test
-	public void testPutExample() throws Exception {
-		Student student = new Student();
-		student.setId(2);
-		student.setName("John");
-		Mockito.when(studentService.updateStudent(ArgumentMatchers.any())).thenReturn(student);
-		String json = mapper.writeValueAsString(student);
-		mockMvc.perform(put("/putMapping").contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-				.content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$.id", Matchers.equalTo(2)))
-				.andExpect(jsonPath("$.name", Matchers.equalTo("John")));
-	}
-
-	@Test
-	public void testDeleteExample() throws Exception {
-		Mockito.when(studentService.deleteStudent(ArgumentMatchers.anyString())).thenReturn("Student is deleted");
-		MvcResult requestResult = mockMvc.perform(delete("/deleteMapping").param("student-id", "1"))
-				.andExpect(status().isOk()).andExpect(status().isOk()).andReturn();
-		String result = requestResult.getResponse().getContentAsString();
-		assertEquals(result, "Student is deleted");
-	}
+	
 }
